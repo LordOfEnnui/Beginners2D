6ddcbb77c3d72bfaf802517cc5b29a4aa0797455
nitro603 <tavaracristian93@gmail.com>
2025-12-28 19:02:11 -0500

    Created Data classes For Modules.json

---
+++ Assets/Scenes/Bababooey/Demo_Assets/Scripts/ModuleDataJSON/ModuleJson.cs
@@ -0,0 +1,13 @@
+using System;
+
+[Serializable]
+public class ModuleJson
+{
+    public string id;
+    public string name;
+    public string description;
+    public float speedMultiplier;
+    public int healthModifier;
+    public string[] abilities;
+    public string rarity;
+}
